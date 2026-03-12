cd37fa5bcbf4055cdfe4319ed0aa1e0f7cda59d0
neumann-davila <neumanndavila@gmail.com>
2026-03-12 12:25:11 -0400

    Migrating plugins to seperate files for easier modification

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function() require('conform').format { async = true, lsp_format = 'fallback' } end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+}
